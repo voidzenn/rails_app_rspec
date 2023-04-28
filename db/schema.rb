4fbebc13d26b0020ec0ce62faa9356aecca0e0b6
michael silverio <michael.silverio@sun-asterisk.com>
2023-04-28 13:57:07 +0800

    Created role model, migration and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_26_073818) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_054902) do
+  create_table "roles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "fname"
     t.string "lname"
