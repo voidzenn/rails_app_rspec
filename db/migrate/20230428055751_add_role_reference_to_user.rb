7f0348149772f799069eb8ac5d80a6d91b87f8e2
michael silverio <michael.silverio@sun-asterisk.com>
2023-04-28 14:01:26 +0800

    Added migration add_role_reference_to_user

---
+++ db/migrate/20230428055751_add_role_reference_to_user.rb
@@ -0,0 +1,5 @@
+class AddRoleReferenceToUser < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :roles, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_28_054902) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_055751) do
   create_table "roles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_roles_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -28,4 +30,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_28_054902) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "roles", "users"
 end
