de19f3e4dd3be6784f0eef6e4b5fd5a894bf9337
mike <mikesil121@gmail.com>
2023-07-13 16:40:03 +0800

    Added validation for Post model title and content

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true,
+            length: { minimum: 3, maximum: 20 }
+  validates :content, presence: true,
+            length: { minimum: 3, maximum: 20 }
 end
