1e98a2e15f736e853961a260474f507ce31bdb83
mike <mikesil121@gmail.com>
2023-07-14 13:23:56 +0800

    Removed spaces users factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     fname {Faker::Name.first_name}
-    lname { Faker::Name.last_name }
+    lname {Faker::Name.last_name}
     age {Faker::Number.between(from: 18, to: 65)}
     location {Faker::Address.city}
     email {Faker::Internet.email}
