d3003c2edd609779feb0d6905f34e62a264410a1
mike <mikesil121@gmail.com>
2023-07-14 13:25:09 +0800

    Removed spaces role model spec

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe Role, type: :model do
   describe "Role validations" do
     describe "#name" do
-      it { is_expected.to validate_presence_of(:name) }
-      it { is_expected.to validate_length_of(:name).is_at_least(4).is_at_most(30) }
+      it {is_expected.to validate_presence_of(:name)}
+      it {is_expected.to validate_length_of(:name).is_at_least(4).is_at_most(30)}
     end
   end
 
   describe "Role associations" do
-    it { is_expected.to have_one(:user).class_name("User") }
+    it {is_expected.to have_one(:user).class_name("User")}
   end
 end
