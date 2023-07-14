96ea9611f60238759a3be4fb146d22a5a15e97e9
mike <mikesil121@gmail.com>
2023-07-14 17:04:39 +0800

    Updated post model spec to follow the standard

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,39 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe "Validations" do
-    describe "validates presence" do
-      describe "#title" do
-        it {is_expected.to validate_presence_of(:title)}
+  describe "validations" do
+    let(:short_characters) {"aa"}
+    let(:long_characters) {Faker::Lorem.sentences(number: 21)}
+
+    describe "#title attribute" do
+      it {is_expected.to validate_presence_of(:title)}
+
+      context "when value is below minimum" do
+        it {is_expected.not_to allow_value(short_characters).for(:title)}
       end
 
-      describe "#content" do
-        it {is_expected.to validate_presence_of(:content)}
+      context "when value is beyond maximum" do
+        it {is_expected.not_to allow_value(long_characters).for(:title)}
       end
     end
 
-    describe "validates length" do
-      let(:short_characters) {"aa"}
-      let(:long_characters) {Faker::Lorem.sentences(number: 21)}
+    describe "#content attribute" do
+      it {is_expected.to validate_presence_of(:content)}
 
-      describe "#title" do
-        context "when value is below minimum" do
-          it {is_expected.not_to allow_value(short_characters).for(:title)}
-        end
-
-        context "when value is beyond maximum" do
-          it {is_expected.not_to allow_value(long_characters).for(:title)}
-        end
+      context "when value is below minimum" do
+        it {is_expected.not_to allow_value(short_characters).for(:title)}
       end
 
-      describe "#content" do
-        context "when value is below minimum" do
-          it {is_expected.not_to allow_value(short_characters).for(:title)}
-        end
-
-        context "when value is beyond maximum" do
-          it {is_expected.not_to allow_value(long_characters).for(:title)}
-        end
+      context "when value is beyond maximum" do
+        it {is_expected.not_to allow_value(long_characters).for(:title)}
       end
     end
   end
